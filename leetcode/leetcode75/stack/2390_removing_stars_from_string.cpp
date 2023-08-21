25bf92b0b5d9fa36f5ccc26ef4d17cd38ffb43d8
V-nsh <vaprohit707@gmail.com>
2023-08-21 19:44:15 +0530

    some explanation and an old problem.

--- leetcode/leetcode75/stack/2390_removing_stars_from_string.cpp
+++ leetcode/leetcode75/stack/2390_removing_stars_from_string.cpp
@@ -6,13 +6,18 @@ using namespace std;
 
 class Solution {
 public:
+    // if we encounter a * we skip it and remove the alphabet just before it
+    // thus leet**code becomes lecode
     string removeStars(string s) {
         vector<char> res;
         for(auto i: s){
             if(i=='*') {
+                // if we encounter * in string, we do not push it and also pop the element just added to the stack.
                 res.pop_back();
-                continue;;
+                // skip the current loop.
+                continue;
             }
+            // otherwise push the character.
             res.push_back(i);
         }
 
@@ -20,6 +25,7 @@ public:
         //     cout << j;
         // }
 
+        // build string from vector
         string str(res.begin(), res.end());
 
         return str;
