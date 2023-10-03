12e8db55415bfdbbae971fa1e437aae7eca850bf
V-nsh <vaprohit707@gmail.com>
2023-10-03 18:26:13 +0530

    reverse Polish aka postfix notation.

--- NeetCode RoadMap/stack/150_evaluate_reverse_polish_notation.cpp
+++ NeetCode RoadMap/stack/150_evaluate_reverse_polish_notation.cpp
@@ -2,13 +2,26 @@
 #include<string>
 #include<stack>
 #include<ctype.h>
+#include<unordered_map>
+#include<functional>
+#include<iostream>
 
 using namespace std;
 
 class Solution {
 public:
+    /*
+    Not like I came up with this but had a similar idea, just didn't know we can add functions to a hashmap.
+    <int (int, int) > means return type int with two parameters int, int.
+    */  
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
+        unordered_map<string, std::function<int (int, int)> > map = {
+            {"+", [] (int a, int b) {return a+b;}},
+            {"-", [] (int a, int b) {return a-b;}},
+            {"*", [] (int a, int b) {return a*b;}},
+            {"/", [] (int a, int b) {return a/b;}},
+        };
         for(string tk: tokens) {
          if (tk != "-" && tk != "+" && tk != "*" && tk != "/")
          {
@@ -19,8 +32,18 @@ public:
             st.pop();
             int second = st.top();
             st.pop();
+            std::function<int(int, int)>  op = map[tk];
+            st.push(op(second, first)); 
          }
-             
         }
+        return st.top();
     }
 };
+
+int main() {
+    vector<string> tokens = {"2","1","+","3","*"};
+    Solution sol;
+
+    cout << sol.evalRPN(tokens);
+    return 0;
+}
